792d02a2fbb337e8a8942388b3954f6d27601b8a
yamara <39893033+yamara-mh@users.noreply.github.com>
2023-03-22 23:05:03 +0900

    Create SEManagerSettings.cs

---
+++ Codes/SEManagerSettings.cs
@@ -0,0 +1,21 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+namespace Yamara
+{
+    [CreateAssetMenu(fileName = nameof(SEManagerSettings), menuName = nameof(ScriptableObject) + "/Create " + nameof(SEManagerSettings))]
+    public class SEManagerSettings : ScriptableObject
+    {
+        [SerializeField] public int MaxAudioSource = 8;
+
+        [SerializeField] public AudioMixerGroup DefaultAudioMixerGroup;
+
+        [SerializeField, Range(0f, 1f)] public float DefaultVolume = 1f;
+
+        [SerializeField] public float DefaultMaxDistance = 50f;
+
+        [SerializeField, Range(0f, 360f)] public float DefaultSpread = 30f;
+
+        [SerializeField, Range(0f, 5f)] public float DefaultDopplerLevel = 1f;
+    }
+}
