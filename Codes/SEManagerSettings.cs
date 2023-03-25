d1f7afacec0986b9308cc093ec597234d6b613a8
yamara <39893033+yamara-mh@users.noreply.github.com>
2023-03-25 13:33:14 +0900

    Update SEManagerSettings.cs

--- Codes/SEManagerSettings.cs
+++ Codes/SEManagerSettings.cs
@@ -6,9 +6,11 @@ namespace Yamara
     [CreateAssetMenu(fileName = nameof(SEManagerSettings), menuName = nameof(ScriptableObject) + "/Create " + nameof(SEManagerSettings))]
     public class SEManagerSettings : ScriptableObject
     {
+        [SerializeField] public AudioSource AudioSourcePrefab = null;
+
         [SerializeField] public int MaxAudioSource = 8;
 
-        [SerializeField] public AudioMixerGroup DefaultAudioMixerGroup;
+        [SerializeField] public AudioMixerGroup DefaultAudioMixerGroup = null;
 
         [SerializeField, Range(0f, 1f)] public float DefaultVolume = 1f;
 
