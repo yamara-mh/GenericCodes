020bd875fa43ad9a798fd0801dc3fc27857b28dd
yamara <39893033+yamara-mh@users.noreply.github.com>
2023-03-25 19:48:43 +0900

    Update SEManagerSettings.cs

--- Codes/SEManagerSettings.cs
+++ Codes/SEManagerSettings.cs
@@ -1,4 +1,3 @@
-using UnityEngine.Audio;
 using UnityEngine;
 
 namespace Yamara
@@ -14,9 +13,5 @@ namespace Yamara
         [SerializeField, Range(0f, 1f)] public float DefaultVolume = 1f;
 
         [SerializeField] public float DefaultMaxDistance = 50f;
-
-        [SerializeField, Range(0f, 360f)] public float DefaultSpread = 30f;
-
-        [SerializeField, Range(0f, 5f)] public float DefaultDopplerLevel = 1f;
     }
 }
