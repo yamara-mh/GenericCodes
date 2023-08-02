adf2df8b6e2162d324946cfb52af3e3f816f71cd
yamara <39893033+yamara-mh@users.noreply.github.com>
2023-08-02 23:03:15 +0900

    Add InterpObservableQueue

--- Codes/PhotonFusionEx.cs
+++ Codes/PhotonFusionEx.cs
@@ -141,7 +141,7 @@ namespace Extensions
         #endregion
 
         #region PlayerRef
-        
+
         public static PlayerRef Host(this NetworkRunner runner) => runner.GameMode == GameMode.Server ? PlayerRef.None : runner.Simulation.Config.DefaultPlayers - 1;
         public static bool IsServerMode(this NetworkRunner runner) => runner.GameMode == GameMode.Server;
 
@@ -222,5 +222,65 @@ namespace Extensions
     public static class PhotonFusionUtil
     {
         public static NetworkRunner Runner => NetworkRunner.Instances.FirstOrDefault();
+    }
+
+    /// <summary>
+    /// A class that delay actions to match the Interpolation Target when the Interpolation DataSource is in Snapshots
+    /// </summary>
+    public class InterpObservableQueue<T>
+    {
+        private NetworkBehaviour nb;
+        private readonly Queue<int> ticks = new();
+        private readonly Queue<T> arguments = new();
+        private Subject<T> subject = new();
+
+        public IObservable<T> Observable => subject;
+
+        /// <summary>
+        /// Please run this function with Spawned()
+        /// </summary>
+        public InterpObservableQueue(NetworkBehaviour networkBehaviour)
+        {
+            nb = networkBehaviour;
+        }
+
+        public void Enqueue(T argument)
+        {
+            if (nb.Runner.IsResimulation) return;
+
+            if (nb.Runner.IsServer || nb.InterpolationDataSource == NetworkBehaviour.InterpolationDataSources.Predicted)
+            {
+                subject.OnNext(argument);
+                return;
+            }
+
+            int tick = nb.Runner.Tick + nb.Runner.Tick - nb.Runner.Simulation.InterpFrom.Tick;
+            ticks.Enqueue(tick);
+            arguments.Enqueue(argument);
+        }
+
+        /// <summary>
+        /// Please run this function with FixedUpdateNetwork()
+        /// </summary>
+        public void UpdateQueues()
+        {
+            if (nb.Runner.IsResimulation) return;
+
+            while (ticks.Count > 0)
+            {
+                if (ticks.Peek() > nb.Runner.Tick) break;
+                ticks.Dequeue();
+                subject.OnNext(arguments.Dequeue());
+            }
+        }
+
+        public void Clear(NetworkBehaviour networkBehaviour = null)
+        {
+            nb = networkBehaviour;
+            ticks.Clear();
+            arguments.Clear();
+            subject.Dispose();
+            subject = new();
+        }
     }
 }
