18bb7aee87e784adb0f2e02f2aa24d85b00929d3
yamara <39893033+yamara-mh@users.noreply.github.com>
2023-08-28 02:24:00 +0900

    Update BorderAudioSourceController.cs

--- Codes/BorderAudioSourceController.cs
+++ Codes/BorderAudioSourceController.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-
 namespace Audio
 {
     public class BorderAudioSourceController : MonoBehaviour
@@ -10,6 +9,7 @@ namespace Audio
         {
             Sphere = 0,
             Cylinder = 1,
+            CylinderInfiniteHeight = 2,
         }
 
         [Header("Border GameObject")]
@@ -17,11 +17,13 @@ namespace Audio
         [SerializeField] private BorderEnum borderType = BorderEnum.Cylinder;
         [SerializeField, Tooltip("Radius when Scale is 1")]
         private float defaultRadius = 0.5f;
+        [SerializeField, Tooltip("Height when Scale is 1")]
+        private float defaultHeight = 1f;
+        [SerializeField, Range(0f, 1f)] private float pivotY = 0.5f;
 
         [Header("AudioSource")]
         [SerializeField] private AudioSource childAudioSource;
         [SerializeField] private bool autoUpdate = true;
-        [SerializeField] private bool alignYPosToListener = true;
 
         [Header("AudioListener")]
         [SerializeField] private Transform listenerTransform;
@@ -67,12 +69,42 @@ namespace Audio
             var vector = listenerTransform.position - centerTransfrom.position;
             if (borderType == BorderEnum.Cylinder) vector.y = 0f;
 
+            switch (borderType)
+            {
+                case BorderEnum.Cylinder:
+                case BorderEnum.CylinderInfiniteHeight:
+                    vector.y = 0f;
+                    break;
+
+                case BorderEnum.Sphere:
+                default:
+                    break;
+            }
+
             Vector3 direction;
             if (vector.sqrMagnitude == 0f) direction = Vector3.zero;
             else direction = vector.normalized;
 
-            var audioPos = centerTransfrom.position + direction * defaultRadius * transform.lossyScale.x;
-            if (alignYPosToListener) audioPos.y = listenerTransform.position.y;
+            var audioPos = centerTransfrom.position + direction * defaultRadius * centerTransfrom.lossyScale.x;
+
+            switch (borderType)
+            {
+                case BorderEnum.Cylinder:
+                    var centerY = centerTransfrom.position.y;
+                    var height = centerTransfrom.lossyScale.y * defaultHeight;
+                    var minY = centerY - height * pivotY;
+                    var maxY = centerY + height * (1f - pivotY);
+                    audioPos.y = Mathf.Clamp(listenerTransform.position.y, minY, maxY);
+                    break;
+                case BorderEnum.CylinderInfiniteHeight:
+                    audioPos.y = listenerTransform.position.y;
+                    break;
+
+                case BorderEnum.Sphere:
+                default:
+                    break;
+            }
+
             audioSourceTransform.position = audioPos;
         }
 #if UNITY_EDITOR
@@ -81,9 +113,13 @@ namespace Audio
             centerTransfrom ??= transform;
             childAudioSource ??= GetComponentInChildren<AudioSource>();
 
-            if (defaultRadius == 0f && centerTransfrom.TryGetComponent(out Renderer r))
+            if (defaultRadius == -1f && centerTransfrom.TryGetComponent(out Renderer r))
             {
                 defaultRadius = r.bounds.size.x / 2f / centerTransfrom.localScale.x;
+            }
+            if (defaultHeight == -1f && centerTransfrom.TryGetComponent(out Renderer r2))
+            {
+                defaultHeight = r2.bounds.size.y / centerTransfrom.localScale.y;
             }
         }
 #endif
